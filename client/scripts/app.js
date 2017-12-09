488b45920054119888c913b0a2bf03ee3b2473a4
francocatania <catania.fco@gmail.com>
2017-12-09 12:31:52 -0800

    add room functionality, send messages, create new room, css changes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   app.init();
 
-  $('#sendForm').on('click', function () {
-
+  $('#sendForm').on('click', function (e) {
+    let theNewMessage = new Message(window.location.search.slice(10) ,
+                          document.getElementById('inputText').value, 
+                          document.getElementById('currentRoom').value );
+    app.send(theNewMessage);
+    app.fetch();
   });
-  $('#roomSelect').on('click', function () {
-    // trigger render room messages
+
+  $('#currentRoom').on('change', function () {  
+    // selected room = null by default, on click sets to current room
+    app.currentRoom = document.getElementById('currentRoom').value;
+    app.fetch();
   });
 
 });
@@ -14,16 +21,20 @@ var app = {
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
   allMessages: [],
   allRooms: [],
+  currentRoom: 'All',
 
   init: function () {
     this.fetch();
+    // setInterval(this.fetch, 5000);
   },
+
+
   send: function (message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
-      data: message,
+      data: JSON.stringify(message),
       contentType: 'application/text',
       success: function (data) {
         console.log('chatterbox: Message sent, siiick');
@@ -35,19 +46,27 @@ var app = {
     });
   },
   fetch: function () {
+    app.clearMessages();
+    
     $.ajax({
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       data: 'order=-createdAt',
       success: function (messages) {
         messages.results.forEach(function(msg) {
-          app.allMessages.push(msg);
+          if (app.currentRoom === 'All') {
+            app.allMessages.push(msg);
+          }
+          if (app.currentRoom === msg.roomname) {
+            app.allMessages.push(msg);
+          }
           if (!!msg.roomname) {
             app.allRooms.push(msg.roomname);
           }
 
         });
-        app.renderAllMessages();
+        app.organizeAllRooms(app.currentRoom);
         app.renderRoomSelect();
+        app.renderAllMessages();
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -55,16 +74,28 @@ var app = {
       }
     });
   },
+// sort array A-Z and bring currentRoom to front
+  organizeAllRooms: function(room) {
+    app.allRooms.push('All');
+    app.uniqRooms();
+    app.allRooms.sort();
+    app.allRooms.splice(app.allRooms.indexOf(room), 1);
+    app.allRooms.unshift(room);
+  },
 
   clearMessages: function() {
     $("#chats").empty();
+    $(".rooms").empty();
+    this.allRooms = [];
+    this.allMessages = [];
   },
 
   renderMessage: function (message) {
     let username = escape(message.username);
     let text = escape(message.text);
     let room = escape(message.roomname);
-    $('#chats').append(`<div>${username} says: ${text}</div>`);
+    let createdAt = message.createdAt;
+    $('#chats').append(`<div class="message">${room}: <div>${username} says:<br><br>${text}</div><br><br>${createdAt}</div>`);
   },
 
   renderAllMessages: function() {
@@ -73,22 +104,14 @@ var app = {
     });
   },
 
-  getAllRooms: function() {
-    // getting a list of all rooms present in your messages
-    //access allMessages --> array
-  },
-  // <option value="volvo">Volvo</option>
-
   uniqRooms: function () {
     app.allRooms = _.uniq(app.allRooms);
   },
 
   renderRoomSelect: function () {
-    debugger;
-    app.uniqRooms()
     app.allRooms.forEach(function (roomNum) {
-      $('.rooms').append(`<option value="${roomNum}">${roomNum}</option>`)
-    })
+      $('.rooms').append(`<option value="${roomNum}">${roomNum}</option>`);
+    });
   }
 };
 
